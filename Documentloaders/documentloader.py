dd42c3673c03882262d828a1402bc5946e9bb94c
mharshitha2503 <mharshitha2503@gmail.com>
2026-05-13 14:55:20 +0530

    Fixed cross-platform document paths

--- Documentloaders/documentloader.py
+++ Documentloaders/documentloader.py
@@ -10,10 +10,10 @@ import os
 
 os.environ["TRANSFORMERS_OFFLINE"] = "1"
 
-# Get absolute path of current file
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Path to docs folder
+
 DOCS_PATH = os.path.join(BASE_DIR, "docs")
 
 
@@ -37,13 +37,17 @@ def allDocs():
     # DOCX files
     docx_files = ["Social Networking Application.docx"]
 
-    for file in docx_files:
-
+    for file in pdf_files:
         file_path = os.path.join(DOCS_PATH, file)
 
-        loader = Docx2txtLoader(file_path)
+    print("PATH:", file_path)
+    print("EXISTS:", os.path.exists(file_path))
 
-        docs.extend(loader.load())
+    loader = PyMuPDFLoader(file_path)
+
+    docs.extend(loader.load())
+
+    
 
     # TEXT files
     txt_files = ["liverdisease.txt", "malaria_detailed.txt"]
