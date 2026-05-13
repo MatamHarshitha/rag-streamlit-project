67a801b7b13e45d162326a6b35ed9ecfe4050359
mharshitha2503 <mharshitha2503@gmail.com>
2026-05-13 14:46:14 +0530

    Fixed cross-platform document paths

--- Documentloaders/documentloader.py
+++ Documentloaders/documentloader.py
@@ -5,39 +5,62 @@ from langchain_community.document_loaders import (
     TextLoader,
     CSVLoader
 )
+
 import os
+
 os.environ["TRANSFORMERS_OFFLINE"] = "1"
 
+# Get absolute path of current file
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Path to docs folder
+DOCS_PATH = os.path.join(BASE_DIR, "docs")
 
 
-def allDocs(path=r"Documentloaders\docs"):
+def allDocs():
+
     docs = []
 
     # PDF files
     pdf_files = ["MLIntro.pdf", "attention.pdf"]
+
     for file in pdf_files:
-        loader = PyMuPDFLoader(os.path.join(path, file))
+
+        file_path = os.path.join(DOCS_PATH, file)
+
+        print(file_path)
+
+        loader = PyMuPDFLoader(file_path)
+
         docs.extend(loader.load())
 
-    # DOCX files 
+    # DOCX files
     docx_files = ["Social Networking Application.docx"]
+
     for file in docx_files:
-        loader = Docx2txtLoader(os.path.join(path, file))
+
+        file_path = os.path.join(DOCS_PATH, file)
+
+        loader = Docx2txtLoader(file_path)
+
         docs.extend(loader.load())
 
     # TEXT files
     txt_files = ["liverdisease.txt", "malaria_detailed.txt"]
+
     for file in txt_files:
-        file_path = os.path.join(path, file)
-        loader = TextLoader(file_path, encoding="utf-8") 
+
+        file_path = os.path.join(DOCS_PATH, file)
+
+        loader = TextLoader(file_path, encoding="utf-8")
+
         docs.extend(loader.load())
 
-    # CSV file
-    csv_loader = CSVLoader(os.path.join(path, "Ads.csv"))
+    # CSV files
+    csv_path = os.path.join(DOCS_PATH, "Ads.csv")
+
+    csv_loader = CSVLoader(csv_path)
+
     docs.extend(csv_loader.load())
 
-    
     return docs
-
-
-
